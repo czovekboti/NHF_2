c451a48304b80eca8e37c17e0aeaa027b566b3d9
czovekboti <czovekboti@gmail.com>
2023-10-12 01:02:19 +0200

    asd

--- src/Main/AiGame.java
+++ src/Main/AiGame.java
@@ -6,11 +6,14 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class AiGame extends Game { 
-	private int round=0;
 	public AiGame(int boardsize) {
         super(boardsize);
-    }
+	}
 	// Játék folyása 
+	public void Opening() {
+		
+	}
+	
 	
 	
 }
